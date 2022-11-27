1f63acf80f5bbd512b754ce127e939d70d0f3709
cvzi <cuzi@openmail.cc>
2022-11-27 23:41:55 +0100

    @icon

--- Show_metacritic_ratings.user.js
+++ Show_metacritic_ratings.user.js
@@ -2,7 +2,7 @@
 // @name             Show Metacritic.com ratings
 // @description      Show metacritic metascore and user ratings on: Bandcamp, Apple Itunes (Music), Amazon (Music,Movies,TV Shows), IMDb (Movies), Google Play (Music, Movies), Steam, Gamespot (PS4, XONE, PC), Rotten Tomatoes, Serienjunkies, BoxOfficeMojo, allmovie.com, fandango.com, Wikipedia (en), themoviedb.org, letterboxd, TVmaze, TVGuide, followshows.com, TheTVDB.com, ConsequenceOfSound, Pitchfork, Last.fm, TVnfo, rateyourmusic.com, GOG, Epic Games Store
 // @namespace        cuzi
-// @icon             https://www.metacritic.com/images/icons/metacritic-icon.svg
+// @icon             https://www.metacritic.com/MC_favicon.png
 // @supportURL       https://github.com/cvzi/Metacritic-userscript/issues
 // @updateURL        https://openuserjs.org/meta/cuzi/Show_Metacritic.com_ratings.meta.js
 // @contributionURL  https://buymeacoff.ee/cuzi
